396060ecaf70f3dd229f378a448cde56877ed541
Alyssa Yates <bosslys@icloud.com>
2019-08-20 00:03:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,5 @@ second_number = 3
 # Add your variables here
 sum = (first_number + second_number)
 difference = (first_number - second_number)
+product = (first_number * second_number)
+quotient = (first_number / second_number)
